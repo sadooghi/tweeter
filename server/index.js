bf7837389af4a979b696cc2f1608764299832e0d
sadooghi <gh.sadooghi@gmail.com>
2017-03-25 22:43:43 +0000

    changed port

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const PORT          = process.env.PORT || 5000;
+const PORT          = process.env.PORT || 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
